ffb188416ba1325e7ee44650c171d0d2b756c6db
peterzhenghj <119376536+peterzhenghj@users.noreply.github.com>
2024-09-25 09:30:06 +0800

    add Port LCKFB_ESP32S3_N8R8

---
+++ ports/espressif/boards/lckfb_esp32s3_n8r8/mpconfigboard.h
@@ -0,0 +1,12 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2019 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Micropython setup
+
+#define MICROPY_HW_BOARD_NAME       "LCKFB ESP32S3-N8R8"
+#define MICROPY_HW_MCU_NAME         "ESP32S3"
