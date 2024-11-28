87417659a8bfd8387ec767cf254d53dad413e1a2
peterzhenghj <119376536+peterzhenghj@users.noreply.github.com>
2024-11-28 09:18:18 +0800

    NEW OFF SET 50-100

--- ports/espressif/boards/waveshare_esp32_c6_lcd_1_47/board.c
+++ ports/espressif/boards/waveshare_esp32_c6_lcd_1_47/board.c
@@ -62,8 +62,8 @@ static void display_init(void) {
         bus,
         320,            // width (after rotation)
         172,            // height (after rotation)
-        100,             // column start
-        50,             // row start
+        50,             // column start
+        100,             // row start
         90, // rotation
         16, // Color depth
         false, // Grayscale
