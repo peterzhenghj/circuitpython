96d3bf75cc47cf1f14aeb29e8791faad6a7349f1
peterzhenghj <119376536+peterzhenghj@users.noreply.github.com>
2025-05-20 16:32:01 +0800

    1 pin

--- ports/stm/boards/stm32f429i_discovery/pins.c
+++ ports/stm/boards/stm32f429i_discovery/pins.c
@@ -9,7 +9,7 @@
 
 static const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
-
+        { MP_ROM_QSTR(MP_QSTR_ID), MP_ROM_PTR(&board_module_id_obj) },
     { MP_ROM_QSTR(MP_QSTR_PE02), MP_ROM_PTR(&pin_PE02) },
     { MP_ROM_QSTR(MP_QSTR_PE03), MP_ROM_PTR(&pin_PE03) },
     { MP_ROM_QSTR(MP_QSTR_PE04), MP_ROM_PTR(&pin_PE04) },
