05780a03d2cb9f53a7476fb7997e6f9f6358ec89
peterzhenghj <119376536+peterzhenghj@users.noreply.github.com>
2025-05-20 13:47:56 +0800

    update pins

--- ports/stm/peripherals/pins.h
+++ ports/stm/peripherals/pins.h
@@ -79,6 +79,9 @@ extern const mp_obj_type_t mcu_pin_type;
 #ifdef STM32F407xx
 #include "stm32f4/stm32f407xx/pins.h"
 #endif
+#ifdef STM32F429xx
+#include "stm32f4/stm32f429xx/pins.h"
+#endif
 #ifdef STM32F446xx
 #include "stm32f4/stm32f446xx/pins.h"
 #endif
