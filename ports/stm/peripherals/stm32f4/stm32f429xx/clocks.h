93d3bc88b53ccb64254c23be72504276321d15de
peterzhenghj <119376536+peterzhenghj@users.noreply.github.com>
2025-05-22 13:44:20 +0800

    update clocks.h format

--- ports/stm/peripherals/stm32f4/stm32f429xx/clocks.h
+++ ports/stm/peripherals/stm32f4/stm32f429xx/clocks.h
@@ -10,55 +10,42 @@
 #ifndef CPY_CLK_VSCALE
 #define CPY_CLK_VSCALE (PWR_REGULATOR_VOLTAGE_SCALE1)
 #endif
-
 #ifndef CPY_CLK_PLLN
 #define CPY_CLK_PLLN (336)
 #endif
-
 #ifndef CPY_CLK_PLLP
 #define CPY_CLK_PLLP (RCC_PLLP_DIV2)
 #endif
-
 #ifndef CPY_CLK_PLLQ
 #define CPY_CLK_PLLQ (7)
 #endif
-
 #ifndef CPY_CLK_PLLR
 #define CPY_CLK_PLLR (2)
 #endif
-
 #ifndef CPY_CLK_PLLI2SM
 #define CPY_CLK_PLLI2SM (HSE_VALUE / 1000000)
 #endif
-
 #ifndef CPY_CLK_AHBDIV
 #define CPY_CLK_AHBDIV (RCC_SYSCLK_DIV1)
 #endif
-
 #ifndef CPY_CLK_APB1DIV
 #define CPY_CLK_APB1DIV (RCC_HCLK_DIV2)
 #endif
-
 #ifndef CPY_CLK_APB2DIV
 #define CPY_CLK_APB2DIV (RCC_HCLK_DIV1)
 #endif
-
 #ifndef CPY_CLK_FLASH_LATENCY
 #define CPY_CLK_FLASH_LATENCY (FLASH_LATENCY_3)
 #endif
-
 #ifndef CPY_CLK_USB_USES_AUDIOPLL
 #define CPY_CLK_USB_USES_AUDIOPLL (1)
 #endif
-
 #ifndef BOARD_HSE_SOURCE
 #define BOARD_HSE_SOURCE (RCC_HSE_ON)
 #endif
-
 #ifndef RCC_PERIPHCLK_CLK48
 #define RCC_PERIPHCLK_CLK48 RCC_PERIPHCLK_CK48
 #endif
-
 #ifndef RCC_CK48CLKSOURCE_PLLI2SQ
 #define RCC_CK48CLKSOURCE_PLLI2SQ RCC_CLK48CLKSOURCE_PLLI2SQ
 #endif
